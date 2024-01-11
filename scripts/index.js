8b392e2108b6aaa00d586abfc552f8235fb7a277
Quang Nguyen <quangnguyen-groove@groovetechnology.com>
2024-01-11 17:21:11 +0700

    A

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,54 @@
-function sendCommand(command) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST','/sendCommand', true)
-    ;xhr.send("data="+command);
-}
-console.log("hello")
+$(function(){
+    
+    const COMMAND={
+        Forward:"F",
+        Backward:"B",
+        Left:"L",
+        Stop:"S",
+        Right:"R",
+    }
+    function sendCommand(cmd){
+        $.ajax({
+            type:"GET",
+            url:"http://192.168.1.7/sendCommand/"+cmd,
+            success:function(response){
+                if(response.ok){
+                    toggleMessage("Thành công")
+                }
+            }
+        })
+    }
+    function toggleMessage(message){
+        $("#divMessage").html(message).show();
+        setTimeout(() => {
+            $("#divMessage").hide();
+        }, 5000);
+    }
+    function initUI(){
+        const htmlString=` <center>
+        <h1>Control Arduno Car</h1>
+        <div class="mt-4 d-flex flex-column">
+            <div> <button id="btnForward" class="btn btn-success btn-lg">Forward</button></div>
+
+            <div class="mt-5"> <button id="btnLeft" class="btn btn-success  btn-lg">Left</button>
+                <button id="btnStop" class="btn btn-success mx-5 btn-lg">Stop</button>
+                <button id="btnRight" class="btn btn-success btn-lg">Right</button>
+            </div>
+            <div class="mt-5">
+                <button id="btnBackWard" class="btn btn-success btn-lg">Backward</button>
+            </div>
+        </div>
+    </center>`
+    $("#app").html(htmlString);
+    }
+   
+    $(document).ready(function(){
+        initUI();
+        $("#btnForward").on("click",function(){sendCommand(COMMAND.Forward)})
+        $("#btnBackward").on("click",function(){sendCommand(COMMAND.Backward)})
+        $("#btnLeft").on("click",function(){sendCommand(COMMAND.Left)})
+        $("#btnRight").on("click",function(){sendCommand(COMMAND.Right)})
+        $("#btnStop").on("click",function(){sendCommand(COMMAND.Stop)})
+    })
+
+})
